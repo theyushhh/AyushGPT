b0719a17dbf5f59850732b21d450730e5fc13194
theyushhh <theyushhh@gmail.com>
2025-07-27 05:06:11 +0530

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
+  base: '/AyushGPT/', // Match your repository name
   build: {
-    outDir: '../docs',
-    emptyOutDir: true
-  }
+    outDir: '../docs', // Output folder for GitHub Pages
+    emptyOutDir: true,
+  },
 })
